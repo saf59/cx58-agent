16a1e14edf35e25d3443cf2b3986bddb303dd3bf
alsh <shpirkov@gmail.com>
2025-12-11 16:43:18 +0200

    drop old

--- build.cmd
+++
@@ -1,5 +0,0 @@
-﻿set RUST_BACKTRACE=full
-set path=%path%;C:\msys64\ 
-@rem cmake
-@cargo build --release
-cargo clippy
--- old/7/src/lib.rs
+++
@@ -1,4 +0,0 @@
-pub mod agent;
-pub mod models;
-pub mod rig_integration;
-pub mod storage;
